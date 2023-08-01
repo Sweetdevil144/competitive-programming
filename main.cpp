8a0feaf8294452d514be44d7d7833a8edd549168
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-08-01 23:10:29 +0530

    Day-0

--- main.cpp
+++ main.cpp
@@ -5,21 +5,9 @@ using namespace std;
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    int x;
-    cin >> x;
-    cin.ignore();
-//     getline() is used to get a String line input;
-    while(x--) {
-        string s1;
-        getline(cin, s1);
-        cout << s1 << '\n';
-    }
-    int a=0,b=10;
-    loop(i,a,b) {
-        cout << i << "  ";
-    }
-//    Using the command typedef it is possible to give a shorter name to a datatype. For example, the name long long is long, so we can define a shorter name ll:
-    typedef long long ll;
+    int a=4,b=10;
+    cout << a << "  " << b << '\n';
+    swap(a,b);
+    cout << a << "  " << b;
     return 0;
 }
